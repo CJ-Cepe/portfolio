0ca7bd68d02eff4a8dc9eff50bf00998b18167ad
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-15 21:12:34 +0800

    Feat: create & add nav component

---
+++ src/Nav.jsx
@@ -0,0 +1,15 @@
+function Nav(){
+    return (
+        <nav>
+            <div>CEPE</div>
+            <ul>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#"></a>Skills</li>
+                <li><a href="#"></a>Projects</li>
+                <li><a href="#"></a>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
