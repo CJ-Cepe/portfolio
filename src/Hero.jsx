f318c62c63e1ea121da0fd5393b66311f8b3fb66
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-15 21:13:18 +0800

    Feat: create & add hero section

---
+++ src/Hero.jsx
@@ -0,0 +1,12 @@
+
+function Hero(){
+    return (
+        <section className="hero">
+            <h1>Hello, I'm <em>Carl Joseph Cepe</em></h1>
+            <p>Computer Science Graduate | Aspiring Web Developer & UI/UX Enthusiast | Actively Seeking Opportunities</p>
+            <button>Resume</button>
+        </section>
+    )
+}
+
+export default Hero
