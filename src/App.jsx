958f47bac3b86f9ea113273b2a6dff1e9fded8f7
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-16 15:45:17 +0800

    Refactor: update import path of css files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Nav from './Nav'
 import Home from './Hero'
 import About from './About'
-import './App.css'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
