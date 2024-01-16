337c35cf968387f487b6cdf67bc1a623e8e9fcd3
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-16 19:25:31 +0800

    Feat: add ! & style hero section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,8 +1,9 @@
+import './styles/Hero.css'
 
 function Hero(){
     return (
         <section className="hero">
-            <h1>Hello, I'm <em>Carl Joseph Cepe</em></h1>
+            <h1>Hello!, I'm <em>Carl Joseph Cepe</em></h1>
             <p>Computer Science Graduate | Aspiring Web Developer & UI/UX Enthusiast | Actively Seeking Opportunities</p>
             <button>Resume</button>
         </section>
