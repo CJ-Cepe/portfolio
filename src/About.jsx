92b618b0a9f8dd0acd4bbe1853354da950960c92
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-19 01:30:31 +0800

    Refactor: move components of AboutSection file into About file

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,44 @@
 import './styles/About.css'
 import {About as data} from './info.json'
-import {Experience, Education, Training} from './AboutSection'
+
+function Experience() {
+    const {main, secondary, location, date, details} = data.experience
+    const detailList = details.map((detail, index)=>
+        <li key={index}>{detail}</li>
+        )
+    return (
+        <div className="about-section">
+            <h2>{main} <span>{location}</span></h2>
+            <h3>{secondary} <span>{date}</span></h3>
+            <ul>{detailList}</ul>
+        </div>
+    )
+}
+
+function Education() {
+    const {main, secondary, location, date, details} = data.education
+    const detailList = details.map((detail, index)=>
+        <li key={index}>{detail}</li>
+        )
+    return (
+        <div className="about-section">
+            <h2>{main} <span>{location}</span></h2>
+            <h3>{secondary} <span>{date}</span></h3>
+            <ul>{detailList}</ul>
+        </div>
+    )
+}
+
+function Training(){
+    const details = data.training.map((detail, index)=>
+        <li key={index}>{detail.name} <span>{detail.date}</span></li>
+        )
+    return (
+        <div className="about-section">
+            <ul>{details}</ul>
+        </div>
+    )
+}
 
 function About(){
     return (
@@ -10,9 +48,9 @@ function About(){
                 <p>{data.background.firstParagraph} <span>{data.background.secondParagraph}</span></p>
             </div>
             <div>
-                <Experience content={data}/>
-                <Education content={data}/>
-                <Training content={data}/>
+                <Experience/>
+                <Education/>
+                <Training/>
             </div>
         </div>
     )
