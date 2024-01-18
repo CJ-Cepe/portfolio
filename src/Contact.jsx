bd94b6fd6d1452be8d60f91b70ed00298c14ac36
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-19 01:47:21 +0800

    Feat: create contact section

---
+++ src/Contact.jsx
@@ -0,0 +1,16 @@
+function Contact(){
+    return (
+        <section>
+            <h2>Philippines</h2>
+            <h3>Antipolo City, 1870</h3>
+            <ul>
+                <li><a>+639205090839</a></li>
+                <li><a href="">cj.ceps@gmail.com</a></li>
+                <li><a href="">github.com/CJ-Cepe</a></li>
+                <li><a href="">linkedin.com/in/cjcepe</a></li>
+            </ul>
+        </section>
+    )
+}
+
+export default Contact
