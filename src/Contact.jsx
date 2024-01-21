b046228410bffafec846b66f1a8a93aad9ed7f08
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-22 00:35:37 +0800

    Feat: change structure html structure - add 2 inner divs & rotating text & adjust some stylings

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,16 +1,21 @@
 import './styles/Contact.css'
-
+import circularText from './assets/circular-text.svg'
 function Contact(){
     return (
         <section className='contact'>
-            <h2>Philippines</h2>
-            <h3>Antipolo City, 1870</h3>
-            <ul>
-                <li><a>+639205090839</a></li>
-                <li><a href="">cj.ceps@gmail.com</a></li>
-                <li><a href="">github.com/CJ-Cepe</a></li>
-                <li><a href="">linkedin.com/in/cjcepe</a></li>
-            </ul>
+            <div>
+                <h2>Philippines</h2>
+                <h3>Antipolo City, 1870</h3>
+                <ul>
+                    <li><a>+639205090839</a></li>
+                    <li><a href="">cj.ceps@gmail.com</a></li>
+                    <li><a href="">github.com/CJ-Cepe</a></li>
+                    <li><a href="">linkedin.com/in/cjcepe</a></li>
+                </ul>
+            </div>
+            <div>
+                <img src={circularText} alt="Open For Work" />
+            </div>
         </section>
     )
 }
