b013f677e355803f26145c71cf9278b5aba9fa8c
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-21 23:29:49 +0800

    Feat: create and add marquee component

---
+++ src/Marquee.jsx
@@ -0,0 +1,30 @@
+import './styles/Marquee.css'
+
+
+function Marquee(){
+    return (
+        <div className="marquee">
+            <ul className="marquee-content" aria-hidden="true">
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+            </ul>
+
+            <ul className="marquee-content" aria-hidden="true">
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+                <li>Open for Job Opportunities! 📣</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Marquee
