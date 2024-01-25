76d8adc1695d12583b0d28624d8fcb466655f57e
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-25 22:35:14 +0800

    Feat: set intersected equivalent section of nav list active

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -8,7 +8,7 @@ function Nav(){
     const titleRef = useRef(null)
     const buttonRef = useRef(null)
     const ulRef = useRef(null)
-
+    
     const navObserver = new IntersectionObserver((entries) => {
         console.log(entries[0].isIntersecting)
         navRef.current.classList.toggle('sticky', !entries[0].isIntersecting)
@@ -16,22 +16,43 @@ function Nav(){
         buttonRef.current.classList.toggle('hide', !entries[0].isIntersecting)
         ulRef.current.classList.toggle('styled-list', !entries[0].isIntersecting)
     }, {rootMargin: "20px 0px 0px 0px"})
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          const id = entry.target.getAttribute('id')
+          ulRef.current.querySelector(`[href="#${id}"]`).parentNode.classList.toggle('active', entry.isIntersecting);
+          /* if (entry.isIntersecting) {
+                ulRef.current.querySelector(`[href="#${id}"]`).classList.add('active');
+            } else {
+                ulRef.current.querySelector(`[href="#${id}"]`).classList.remove('active');
+            } */
+        });
+    }, { threshold: 0.1 });
+
     
     useEffect(()=> {
-        //run once
-        console.log(scrollWatcher.current)
+        //give navObserver observe
         if(scrollWatcher.current){
             navObserver.observe(scrollWatcher.current)
         }
 
+        //give sections observer
+        const sections = document.querySelectorAll("#about-section, #skills-section, #projects-section, #contact-section")
+        sections.forEach(section => observer.observe(section));
+            
+        
          // Cleanup: stop observing when component unmounts since array is empty
         return () => {
             if (scrollWatcher.current) {
                 navObserver.unobserve(scrollWatcher.current);
+                sections.forEach(section => observer.unobserve(section));
             }
         }
     }, [])
-    
+
+
+  
+   
     return (
         <>
         <div ref={scrollWatcher} aria-hidden="true"></div>
@@ -40,7 +61,7 @@ function Nav(){
             <ul ref={ulRef}>
                 <li><a href="#about-section">About</a></li>
                 <li><a href="#skills-section">Skills</a></li>
-                <li className='active'><a href="#projects-section">Projects</a></li>
+                <li><a href="#projects-section">Projects</a></li>
                 <li><a href="#contact-section">Contact</a></li>
             </ul>
             <button ref={buttonRef}><img src={sun} alt="sun icon for lightmode" /></button>
