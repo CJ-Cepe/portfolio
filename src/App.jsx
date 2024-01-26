8a14dd7d784867dca93e24f3bdb319f11c43ed87
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-27 02:46:34 +0800

    Feat: change page color theme based on user's system preference

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,33 @@ import Projects from './Projects'
 import Marquee from './Marquee'
 import './styles/App.css'
 
-function App() {
-  document.querySelector('#root').setAttribute('data-theme', 'light');
+function App() { 
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    document.querySelector('#root').setAttribute('data-theme', 'light');
+    console.log('prefers light')
+  } else {
+    document.querySelector('#root').setAttribute('data-theme', 'dark');
+    console.log('prefers dark')
+  }
   
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const systemTheme = event.matches ? 'dark' : 'light';
+    document.querySelector('#root').setAttribute('data-theme', systemTheme);
+    console.log('theme change')
+  });
+
+  const selectedTheme = localStorage.getItem('selectedTheme');
+
+  if(!selectedTheme){
+    selectedTheme = 'light';
+    localStorage.setItem('selectedTheme', selectedTheme)
+  }
+
+  //apply theme to docu
+
+
+  
+
   return (
     <>
     <Nav/>
