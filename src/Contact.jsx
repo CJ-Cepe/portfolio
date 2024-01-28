c6c8909d63b9726d8d11b002ea593d7d81bd0201
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-29 03:13:20 +0800

    Feat: add animation when intersected

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,18 +1,42 @@
+import { useRef, useEffect } from 'react'
 import './styles/Contact.css'
 import circularText from './assets/circular-text.svg'
+
 function Contact(){
+    const contactRef = useRef(null)
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                entry.target.classList.add('appear');
+            }
+        })
+    }, {threshold: 0.1, rootMargin: "0px 0px -150px 0px"})
+
+    useEffect(() => {
+        const segments = contactRef.current.querySelectorAll('.fade-in')
+        if(segments){
+            segments.forEach(segment => {observer.observe(segment)})
+        }
+
+        return () => {
+            if(segments){
+                segments.forEach(segment => {observer.unobserve(segment)})
+            }
+        }
+    }, [])
+
     return (
-        <section id="contact-section" className='contact'>
+        <section id="contact-section" ref={contactRef}>
             <div>
-                <p>Philippines</p>
-                <p>Antipolo City, 1870</p>
-                <a>+639205090839</a>
-                <a href='#'>cj.ceps@gmail.com</a>
-                <a href='#'>github.com/CJ-Cepe</a>
-                <a href='#'>linkedin.com/in/cjcepe</a>
+                <p className='fade-in'>Philippines</p>
+                <p className='fade-in'>Antipolo City, 1870</p>
+                <a className='fade-in'>+639205090839</a>
+                <a className='fade-in' href='#'>cj.ceps@gmail.com</a>
+                <a className='fade-in' href='#'>github.com/CJ-Cepe</a>
+                <a className='fade-in' href='#'>linkedin.com/in/cjcepe</a>
             </div>
             <div>
-                <img src={circularText} alt="Open For Work" />
+                <img className='fade-in' src={circularText} alt="Open For Work" />
             </div>
         </section>
     )
