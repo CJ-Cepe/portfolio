cca5f87d2e62ab93353a8d4e4cd74d00aee83ad2
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-29 20:14:28 +0800

    Feat: add scroll restoration to return to the top in reload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Nav from './Nav'
+import Hero from './Hero'
+import About from './About'
+import Skills from './Skills'
+import Contact from './Contact'
+import Projects from './Projects'
+import Marquee from './Marquee'
+import './styles/App.css'
+import './styles/animation.css'
+import { useEffect } from 'react'
+
+function App() { 
+  useEffect(() => {
+    window.history.scrollRestoration = 'manual'
+  }, []);
+
+  return (
+    <>
+    <Nav/>
+    <main>
+      <Hero/>
+      <div>
+        <Marquee/>
+        <About/>
+        <Skills/>
+        <Projects/>
+        <Contact/>
+        <footer>
+          Copyright©2024, CJ-Cepe
+        </footer>
+      </div>
+    </main>
+    
+    </>
+  )
+}
+
+export default App
