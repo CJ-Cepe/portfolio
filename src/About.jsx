23bfbc4a388b9efae9775864cab6b42c2a5c34eb
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-01-30 16:51:29 +0800

    Feat: add spline component

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,10 @@
 import './styles/About.css'
 import {About as data} from './info.json'
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 import useIntersectionObserver from './useIntersectionObserver'
+
+import React, { Suspense } from 'react';
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 
 function Experience() {
@@ -50,11 +53,38 @@ function About(){
     const aboutRef = useRef(null)
     useIntersectionObserver(aboutRef, 'pre-appear', 'appear-up')
 
+    function onLoad(spline) {
+        console.log('printed')
+       /*  spline.setZoom(-10)
+        spline.setSize(400, 400)
+       const obj = spline.findObjectById('07a8eb7e-3b9f-4eb4-86dd-7229f9d1fc60'); */
+       
+      }
+
+    useEffect(() => {
+        /* const script = document.createElement('script');
+        script.type = "module"
+        script.src = 'https://unpkg.com/@splinetool/viewer@1.0.38/build/spline-viewer.js';
+        script.async = true;
+        document.querySelector('#about-section > div:first-child').appendChild(script);
+     */
+
+       /*  const canvas = document.getElementById('canvas3d');
+        const app = new Application(canvas);
+        app.load('https://prod.spline.design/fN9iccxUvAx3YMg6/scene.splinecode', 
+            undefined, { credentials: 'include', mode: 'no-cors', }); */
+            
+        return () => {
+            /* document.querySelector('#about-section > div:first-child').removeChild(script); */
+        }
+      }, []);
     
     return (
         <section id="about-section" className="about" ref={aboutRef}>
             <div>
-                <img src="" alt="" />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <div><Spline scene="./src/assets/scene.splinecode" onLoad={onLoad}/></div>
+                </Suspense>
                 <p className='pre-appear'>{data.background.firstParagraph}</p>
                 <p className='pre-appear'>{data.background.secondParagraph}</p>
             </div>
